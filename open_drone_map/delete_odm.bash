e1464248421d0f10353a5a2db15f7d5da6b8df0b
Alex Broz <atbroz@vt.edu>
2021-12-05 18:50:31 -0500

    odm stitching

---
+++ open_drone_map/delete_odm.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo rm -rf odm_dem/ odm_meshing/ odm_filterpoints/ odm_orthophoto/ odm_texturing_25d/ odm_georeferencing/ odm_report/ opensfm/
+sudo rm cameras.json images.json img_list.txt log.json
